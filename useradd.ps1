83dd8d0a58651abf9c933fc13b410f930a4c49fb
Sergei Pavlenkov <sergei.pavlenkov@gmail.com>
2023-12-07 13:52:55 -0800

    Kasutajate sktipt-praktika

---
+++ useradd.ps1
@@ -0,0 +1,15 @@
+﻿#Username and password creating
+$user=Read-Host "Sisesta palun oma nimi"
+$password=ConvertTo-SecureString "ParoolAvatudTekstina" -AsPlainText -Force
+$accountname=Read-Host "Sisesta palun oma kasutajanimi"
+$description=Read-Host "Sisesta palun oma kasutajanimi"
+Write-Host"Kasutaja $accountname on loodud"
+
+#Kasutaja lisamine 
+param(
+$user,
+$password,
+$accountname,
+$description
+)
+New-LocalUser $user  -Password $password -FullName $user  -Description $title
